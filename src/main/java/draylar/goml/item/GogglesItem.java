2364052149fdd7f5b4e1ae230045f472d3e65e52
ZekeZ <contact@zekez.dev>
2026-02-17 12:07:20 +1000

    fix imports and update to 1.21.11 + mojmap

--- src/main/java/draylar/goml/item/GogglesItem.java
+++ src/main/java/draylar/goml/item/GogglesItem.java
@@ -1,28 +1,21 @@
 package draylar.goml.item;
 
-import draylar.goml.GetOffMyLawn;
 import draylar.goml.api.ClaimUtils;
-import draylar.goml.api.WorldParticleUtils;
 import eu.pb4.polymer.core.api.item.PolymerItem;
 import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.resources.Identifier;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
-import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.equipment.ArmorMaterials;
 import net.minecraft.world.item.equipment.ArmorType;
 import org.jetbrains.annotations.Nullable;
 import xyz.nucleoid.packettweaker.PacketContext;
-
-import java.util.stream.Collectors;
 
 public class GogglesItem extends Item implements PolymerItem {
     public GogglesItem(Properties settings) {
@@ -39,9 +32,7 @@ public class GogglesItem extends Item implements PolymerItem {
                         world,
                         entity.blockPosition().offset(-distance, -distance, -distance),
                         entity.blockPosition().offset(distance, distance, distance)).forEach(
-                        claim -> {
-                            ClaimUtils.drawClaimInWorld(player, claim.getValue());
-                        });
+                        claim -> ClaimUtils.drawClaimInWorld(player, claim.getValue()));
             }
         }
     }
